033d2eec304f554c7d0c47ef13cd591b470e7eeb
Kevin Choe <choe29@purdue.edu>
2020-12-27 17:55:33 -0800

    Add files via upload

---
+++ project01.R
@@ -0,0 +1,29 @@
+# Kevin Choe
+# Project 1
+
+#1. The memory on the 7 frontend nodes is 4 * 512 + 3 * 768 = 4352 GB = 4.3 TB
+#   The memory on the 28 nodes in the sub-cluster is: 24 * 64 + 4 * 192 = 2304 GB = 2.3 TB
+#   The memory on Kevin's laptop is 8 GB.
+
+#2 I am working on Scholar front end number 0.
+system("hostname")
+
+
+#3. "You've successfully loaded The Data Mine R settings!"
+
+#4 There are 3 chunks of R code, 1 chunk of bash, 1 chunk of Python, 1 chunk of SQL
+
+#5. 
+#Output displayed is: 1, 2, 3
+my_variable <- c(1,2,3)
+my_variable
+
+#6 
+4 * 512 + 3 * 768
+24 * 64 + 4 * 192
+
+#7 
+splash_mountain <- read.csv("/class/datamine/data/disney/splash_mountain.csv")
+head(splash_mountain)
+
+#8
